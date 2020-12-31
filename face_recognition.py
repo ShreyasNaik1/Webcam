9acc640a857470f8254b28fc47bdae2e55881be3
Lord Vader <shreyas.naik111@gmail.com>
2020-12-31 04:57:45 -0800

    hopefully works

--- face_recognition.py
+++ face_recognition.py
@@ -1,36 +1,37 @@
 import cv2
 import sys
+import feature_recognition_window as frw
 
-cascPath = None
-if sys.argv[1]:
-    cascPath = sys.argv[1]
-else: 
-    cascPath = None
+def main():
+    frw.func()
+    casc = frw.get_ret()
+    if not casc:
+        return
 
+    feature_cascade = cv2.CascadeClassifier(casc)
+    vid = cv2.VideoCapture(0)
 
+    while True:
+        _, frame = vid.read()
+        greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-faceCascade = cv2.CascadeClassifier(cascPath)
+        feature = feature_cascade.detectMultiScale(
+            greyscale,
+            scaleFactor=1.1,
+            minNeighbors=10,
+            minSize=(25, 25),
+            flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        )
 
-video_capture = cv2.VideoCapture(0)
+        for (x, y, w, h) in feature:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-while True:
-    ret, frame = video_capture.read()
-    greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    feature = faceCascade.detectMultiScale(
-        greyscale,
-        scaleFactor=1.1,
-        minNeighbors=10,
-        minSize=(25, 25),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+    vid.release()
+    cv2.destroyAllWindows()
 
-    for (x, y, w, h) in feature:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    cv2.imshow('Video', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
